61529248cd4b687391028fc7858b867a7986687d
Luke Whyte <lukeawhyte@gmail.com>
2019-08-15 13:02:55 -0500

    header complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,5 +6,8 @@ import './header.scss';
 export default () => (
     <div className="header">
         <h1>Dual Language Student Performance Explorer</h1>
+        <p className="byline">By <a href="https://www.expressnews.com/author/luke-whyte/" target="_blank">Luke Whyte</a> and <a href="https://www.expressnews.com/author/alia-malik/" target="_blank">Alia Malik</a>, Data updated June 10th, 2019</p>
+        <p className="subhead">Use this tool to evaluate and compare dual language and english language learner student performance in programs throughout Bexar County. You can search for individual schools or use the bubble chart to explore school rankings.</p>
+        <p className="related"><strong>Related story:</strong> <a href="#" target="_blank">Dual language students...</a></p>
     </div>
 )
